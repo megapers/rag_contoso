9a01b0bc9ce3f193c72d453c5505369b23b7598b
Timur Makimov <megapers@gmail.com>
2025-11-11 19:24:55 -0800

    Enable Swagger in Production for demo

--- backEnd/ProductSales/Program.cs
+++ backEnd/ProductSales/Program.cs
@@ -59,11 +59,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Enable Swagger in all environments for demo purposes
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
